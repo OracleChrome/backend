073efeca8341c1dc2c6c579244a84cbb8891ec1a
Kennet Postigo <kennetfpostigo@gmail.com>
2016-08-07 06:42:37 -0400

    updated controller to handle letting user value fallthrough to response.

--- api/controllers/nlp.js
+++ api/controllers/nlp.js
@@ -1,11 +1,11 @@
 const APPID 	= '421d9003';
 const APPKEY 	= '4044146615866869b5a393e0ebae59ad';
 const BINGKEY 	= '83642663acce4d758ac689d5dd56ce70';
-const NLP_PATH 	= 'my_script.py'; 
+const NLP_PATH 	= 'my_script.py';
 
 const Aylien 	= require('aylien_textapi');
 
-const DB 		= require('../models'); 
+const DB 		= require('../models');
 const hydro 	= require('../services/hydrator');
 
 // const shell 	= new python(NLP_PATH);
@@ -45,18 +45,17 @@ module.exports = {
 
 	users(req, res) {
 		var {email, googleId, history} = req.body;
-		console.log('le body', req.body);
+		console.log(email, googleId);
 
-		if(!email || !googleId || history)
+		if(!email || !googleId || history < 1)
 			return res.status(400).send('`users` paylod is missing a property, please try again.');
 
 		//do null checking
 
-		
+
 
 		DB.models.user.findOrCreate({where: {email: email, googleid: googleId}})
-		.then(user => console.log(user))
-		.then(() => {
+		.then((user) => {
 			history.map(h => {
 			h.user_id = user.id;
 			return h;
@@ -67,10 +66,10 @@ module.exports = {
 		});
 
 
-		
+
 		// DB.models.user.findOrCreate({where: {email: email, googleid: googleId}})
 		// 	.then((user) => {
-		// 		hist.user_id = googleId;			
+		// 		hist.user_id = googleId;
 		// 		return DB.models.history.create(hist);
 		// 	})
 		// 	.then(() => res.send('User was successfully added'))
@@ -83,42 +82,42 @@ module.exports = {
 
 		natlan.entities(webpage, (err, data) => res.json(err || [{
 			metadata: {
-				type: 'google-maps', 
-				term: 'Miami', 
+				type: 'google-maps',
+				term: 'Miami',
 				catergory: 'location'},
 			data: 'https://www.google.com/maps?q=maimi'
-		},{ 
+		},{
 			metadata: {
-				type: 'youtube-video', 
-				term: 'Miami', 
+				type: 'youtube-video',
+				term: 'Miami',
 				catergory: 'location'},
 			data: 'https://www.youtube.com/watch?v=XzGLSrgrT2M'
-		},{ 
+		},{
 			metadata: {
-				type: 'youtube-video', 
-				term: 'Miami', 
+				type: 'youtube-video',
+				term: 'Miami',
 				catergory: 'location'},
 			data: 'https://www.youtube.com/watch?v=XzGLSrgrT2M'
-		}, { 
+		}, {
 			metadata: {
-				type: 'twitter-tweet', 
-				term: 'GraphQL', 
+				type: 'twitter-tweet',
+				term: 'GraphQL',
 				catergory: 'location'},
 			data: {
 				author: 'stubailo',
 				tweet: 'Slowly expanding my repository of different ways to generate #GraphQL schemas in JavaScript! https://github.com/apollostack/graphql-syntax … add your own!',
 				authorImage: 'https://pbs.twimg.com/profile_images/711276909544361984/eL3C4-Ui_bigger.jpg'
 			}
-		}, { 
+		}, {
 			metadata: {
-				type: 'wikipedia-information', 
-				term: 'Composition over Inheritance', 
+				type: 'wikipedia-information',
+				term: 'Composition over Inheritance',
 				catergory: 'location'},
 			data: 'https://en.wikipedia.org/wiki/Composition_over_inheritance'
-		},{ 
+		},{
 			metadata: {
-				type: 'yelp-information', 
-				term: 'Barcade', 
+				type: 'yelp-information',
+				term: 'Barcade',
 				catergory: 'location'},
 			data: {
 				name: 'Barcade',
