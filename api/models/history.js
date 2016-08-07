ff4a5ea82cc67014e6e409afab4e4bcb10697047
Kennet Postigo <kennetfpostigo@gmail.com>
2016-08-07 06:41:29 -0400

    changed STRING types into TEXT types.

--- api/models/history.js
+++ api/models/history.js
@@ -1,12 +1,11 @@
 const Sequelize = require('sequelize');
 const sql = require('./index');
-const {STRING, DATE, DOUBLE, INTEGER} = Sequelize;
+const {TEXT, DATE, DOUBLE, INTEGER} = Sequelize;
 
 const History = {
-	url: STRING,
-	user_id: STRING,
-	title: STRING,
-	googleid: STRING,
+	url: TEXT,
+	title: TEXT,
+	googleid: TEXT,
 	last_visit_time: DOUBLE,
 	visit_count: INTEGER,
 	typed_count: INTEGER
